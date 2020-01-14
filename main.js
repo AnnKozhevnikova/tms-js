428092e6e0d26970ab4bd67f0d4a72260fd00bbe
Anna <anna2000_21@mail.ru>
2020-01-14 19:43:17 +0300

    homework5

--- main.js
+++ main.js
@@ -1,100 +1,34 @@
-                 //HOME WORK 1
-let minNo=function (a,b) {
-    if (a<b) {
-        return a;
-    }
-    else if(a===b) {
-        return ('Введите разные значения');
-    }
-    else
-    {
-        return b;
-    }
-    }
+//HOMEWORK 1
+// const sum = (a) => {
+//
+//     return function (b) {
+//         return a+b;
+//     };
+// };
+//
+// console.log(sum(5)(2));
 
 
+//HOMEWORK2 криво
 
-console.log(minNo(6,-8));
+const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
-
-let maxNo=function (a,b) {
-    if (a>b) {
-        return a;
-    }
-    else if(a===b) {
-        return ('Введите разные значения');
-    }
-    else
-    {
-        return b;
-    }
-}
+const text1=document.getElementById('text1');
+const text2=document.getElementById('text2');
+const text3=document.getElementById('text3');
 
 
+text1.addEventListener('click', event => {
+ event.preventDefault();
+ text1.style.color = 'cyan';
+});
 
-console.log(maxNo(-8,-8));
+text2.addEventListener('click', event => {
+ event.preventDefault();
+ text2.style.color = 'magenta';
+});
 
-let MinNo=function(a,b) {
-    return a>b?a:b;
-};
-
-console.log(MinNo(17,1));
-
-                //CLASS WORK
-
- let word='мяу';
-     let arr= word.split('');
-     console.log(arr);
-     let arrR= arr.reverse();
-     console.log(arrR);
-     if (word===arrR.join('')) {
-         console.log('Полиндром!');}
-                 else {
-                      console.log('Не полиндром!');
-                     };
-
-
-
- let pol= function (Word) {
-     let Arr=Word.split('');
-     console.log(Arr);
-     let ArrR= Arr.reverse();
-     console.log(ArrR);
-     if (Word===ArrR.join('')) {
-         return 'Полиндром!'
-     } else {
-         return 'Не полиндром!'
-     }
- }
-
-console.log(pol('шабаш'));
-
-                 //HOMEWORK 2
-
-let change=function(array) {
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] % 10 === 0) {
-            array[i] = array[i].toString();
-            array[i] = array[i].replace(/0/g, 'zero');
-        }
-    }
-    return array;
-};
-let array = [5, 86, 20, 4, 100, 66, 890, 33, 80, 9];
-console.log(array);
-
-console.log(change(array));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+text3.addEventListener('click', event => {
+ event.preventDefault();
+ text3.style.color = 'skyblue';
+});
