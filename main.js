05eacb063f57006a4f290497666d0132ed746fed
Anna <anna2000_21@mail.ru>
2020-01-19 01:42:16 +0300

    symbol.iterator,next.

--- main.js
+++ main.js
@@ -1,41 +1,39 @@
-//HOMEWORK 1
-// const sum = (a) => {
-//
-//     return function (b) {
-//         return a+b;
-//     };
-// };
-//
-// console.log(sum(5)(2));
-
-
-//HOMEWORK2 криво
-
-const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
 
 const text1=document.getElementById('text1');
 const text2=document.getElementById('text2');
 const text3=document.getElementById('text3');
 
-
-
- let f1=() => {
- let i=0;
- return () => {
-  text1.style.color = colors[i];
-  i++;
-  if (i==colors.length){i=0};
+const colors = {
+ data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+ [Symbol.iterator]() {
+  return this;
+ },
+ next(id) {
+  if (this.id === undefined) {
+   this.id = 0;
+  }
+  if (this.id < this.data.length) {
+   return {
+    done: false,
+    value: this.data[this.id++]
+   };
+  } else {
+   this.id=0;
+   return {
+    done: true
+   };
+  }
  }
 }
-text1.addEventListener('click',f1());
+
+const changeStyle = id => event => {
+ event.target.style.color = colors.next(id).value;
+};
+
+ text1.addEventListener('click',changeStyle(Symbol()));
+ text2.addEventListener('click',changeStyle(Symbol()));
+ text3.addEventListener('click',changeStyle(Symbol()));
 
 
-text2.addEventListener('click', event => {
- event.preventDefault();
- text2.style.color = 'magenta';
-});
 
-text3.addEventListener('click', event => {
- event.preventDefault();
- text3.style.color = 'skyblue';
-});
+
